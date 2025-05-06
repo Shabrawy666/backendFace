9f5c30b2a8dd06c3e8dc1867f7b042e7670ad456
Khaled <khaledshbrawy@gmail.com>
2025-05-07 00:26:08 +0300

    editss

--- routes/student.py
+++ routes/student.py
@@ -149,3 +149,23 @@ def get_student_attendance():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@student_bp.route('/<int:student_id>', methods=['GET'])
+@jwt_required()
+def get_student_by_id(student_id):
+    try:
+        student = Student.query.filter_by(student_id=student_id).first()
+
+        if not student:
+            return jsonify({"error": "Student not found"}), 404
+
+        student_data = {
+            "student_id": student.student_id,
+            "name": student.name,
+            "email": student.email,
+            "face_encoding": student.face_encoding,
+             }
+
+        return jsonify(student_data), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
