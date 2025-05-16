bf6d76e36c191a79459748cfc76293e855b8353c
Khaled <khaledshbrawy@gmail.com>
2025-05-16 18:36:44 +0300

    hhhh

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,5 @@
-from ml.liveness import LivenessDetector
-from ml.preprocessing import ImagePreprocessor
-from ml.recognition import FaceRecognitionSystem
+from .ml.liveness import LivenessDetector
+from .ml.preprocessing import ImagePreprocessor
+from .ml.recognition import FaceRecognitionSystem
 
 __all__ = ['LivenessDetector', 'ImagePreprocessor', 'FaceRecognitionSystem']
